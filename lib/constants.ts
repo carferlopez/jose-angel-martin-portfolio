c48da744575ebc3fc75b40babe6dd0b35da0ef21
carferlopez <ferlop.carlos@gmail.com>
2026-06-28 12:24:28 +0200

    feat: replace logo with new Záncara Ohm design with text, and update phone/email constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 // ← Actualiza estos datos antes de publicar
-export const PHONE_RAW = '34600000000'
-export const PHONE_DISPLAY = '600 000 000'
+export const PHONE_RAW = '34605066317'
+export const PHONE_DISPLAY = '605 066 317'
 export const PHONE_HREF = `tel:+${PHONE_RAW}`
-export const EMAIL = 'info@joseangelmartininge.es'
-export const SITE_URL = 'https://joseangelmartininge.es'
+export const EMAIL = 'pepe@zancara.es'
+export const SITE_URL = 'https://zancara.es'
